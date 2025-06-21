b7126f328c92bd1fb8b40262a60827aeaafb6e76
Maria Clara C Soares <claracsoares10@gmail.com>
2025-06-20 22:49:43 -0300

    tentativa 2 de ajeitar foto

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Portfolio Responsive Complete
-## [Watch it on youtube](https://youtu.be/AKNvTxWOdKw)
-### Portfolio Responsive Complete
+# Portfólio Pessoal
 
-- Responsive Personal Portfolio Website HTML CSS & JavaScript.
-- Contains animations when scrolling.
-- Smooth scrolling in each section.
-- Developed first with the Mobile First methodology, then for desktop.
-- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+Este repositório contém o código do meu portfólio pessoal desenvolvido em HTML, CSS e JavaScript.
 
-💙 Join the channel to see more videos like this. [Bedimcode](https://www.youtube.com/@Bedimcode)
+O projeto foi inspirado no template do canal [Bedimcode](https://www.youtube.com/@Bedimcode) e adaptado para apresentar minhas habilidades e projetos.
+
+Para visualizar o site basta abrir `index.html` em seu navegador. Os recursos de estilo e imagem estão na pasta `assets/`.
 
 ![preview img](/preview.png)
-
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const showMenu = (toggleId, navId) =>{
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
         })
-    }
+ }
 }
 showMenu('nav-toggle','nav-menu')
 
@@ -31,15 +31,19 @@ const scrollActive = () =>{
         const sectionHeight = current.offsetHeight,
               sectionTop = current.offsetTop - 58,
               sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }                                                    
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')                                                
+
+        if(sectionsClass){
+            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link')
+            }else{
+                sectionsClass.classList.remove('active-link')
+            }
+        }
     })
 }
+
+scrollActive()
 window.addEventListener('scroll', scrollActive)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
